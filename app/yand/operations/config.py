d2c1b1b122702559a857be0561f7b10e0302235c
Nikita Menshov <nikitatop2222@gmail.com>
2024-08-27 16:53:54 +0500

    Update yt link

--- app/yand/operations/config.py
+++ app/yand/operations/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 current_script_path = os.path.dirname(os.path.abspath(__file__))
-relative_path = os.path.join(current_script_path, '..', '..', 'media') 
+relative_path = os.path.join(current_script_path, '..', '..', 'media')
 FULL_DIR = os.path.abspath(relative_path) + "/"
-YT_LINK = "https://yt.artemislena.eu/search?q="
-GENIUS_API = os.getenv("GENIUS_API")  
-DRIVER = os.getenv("DRIVER") 
+YT_LINK = "https://invidious.jing.rocks/search?q="
+GENIUS_API = os.getenv("GENIUS_API")
+DRIVER = os.getenv("DRIVER")
